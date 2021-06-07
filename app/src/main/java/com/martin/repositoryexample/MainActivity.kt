f6dc63d48fc9b983361cda6a51c5055ca9bc0570
Martin Nguyen <knguyenduy@clever-age.com>
2021-06-07 22:26:06 +0800

    Init

---
+++ app/src/main/java/com/martin/repositoryexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.martin.repositoryexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
